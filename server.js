732377d51804ae9ac609252721313be454392ffe
ALINouman777 <hb6348248@gmail.com>
2023-04-06 18:22:41 +0500

    fix cors problem

--- server.js
+++ server.js
@@ -8,16 +8,23 @@ import cors from "cors"
 
 
 const app= express();
+app.use(function(req, res, next) {
+    // Set the Access-Control-Allow-Origin header to allow requests from all origins
+    res.header('Access-Control-Allow-Origin', '*');
+    // Set the Access-Control-Allow-Headers header to allow the Content-Type header
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
     origin:['https://notes-app-7g2s.onrender.com'],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true,
-}))
+}));
 dotenv.config();
 connectDb();
 
-app.use(cookieParser())
-app.use(express.json())
 app.use("/user", userRoute);
 app.use("/task",Taskroute );
 
