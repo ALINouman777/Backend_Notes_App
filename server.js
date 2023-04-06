a0234cda7d81cf85121d95d60b73e63dd56d7570
ALINouman777 <hb6348248@gmail.com>
2023-04-07 03:47:42 +0500

    fix cors problem

--- server.js
+++ server.js
@@ -7,33 +7,31 @@ import cookieParser from "cookie-parser";
 import cors from "cors"
 
 
-const app= express();
-
-const corsOptions = {
-    origin: ['http://localhost:4000', 'https://notes-app-7g2s.onrender.com'],
-    methods: 'GET,POST,PUT,DELETE',
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization']
-  };
+const app = express();
 
 dotenv.config();
 connectDb();
 app.use(express.json());
 app.use(cookieParser());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://notes-app-7g2s.onrender.com");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://notes-app-7g2s.onrender.com");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
+app.use(cors(
+    {
 
-app.use(cors(corsOptions));
-
+        origin: [process.env.FRONT_URL],
+        methods:['GET','POST',"PUT",'DELETE'],
+        credentials:true
+    }
+));
 app.use("/user", userRoute);
-app.use("/task",Taskroute );
+app.use("/task", Taskroute);
 
 
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT, () => {
     console.log(`server is listening in ${process.env.MODE} `)
 })
