26ade590cd82056a4e0025d0bd13650c091cba16
ALINouman777 <hb6348248@gmail.com>
2023-04-06 19:16:46 +0500

    fix cors problem

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ import cookieParser from "cookie-parser";
 import cors from "cors"
 
 
+connectDb();
+dotenv.config();
 const app= express();
+app.use(express.json());
+app.use(cookieParser());
 app.use(function(req, res, next) {
     // Set the Access-Control-Allow-Origin header to allow requests from all origins
     res.header('Access-Control-Allow-Origin', 'https://notes-app-7g2s.onrender.com');
@@ -15,15 +19,12 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
   });
-app.use(express.json());
-app.use(cookieParser());
+
 app.use(cors({
     origin:['https://notes-app-7g2s.onrender.com'],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true,
 }));
-dotenv.config();
-connectDb();
 
 app.use("/user", userRoute);
 app.use("/task",Taskroute );
