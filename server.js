d1fbe38e4071386bae422814f9a9409eb02683b7
ALINouman777 <hb6348248@gmail.com>
2023-04-07 02:58:09 +0500

    fix cors problem

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ dotenv.config();
 connectDb();
 app.use(express.json());
 app.use(cookieParser());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://notes-app-7g2s.onrender.com");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(cors());
 
 app.use("/user", userRoute);
